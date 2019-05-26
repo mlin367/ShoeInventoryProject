960b351070bc9bfa28c5b86c1381c8472e0b11c0
Matthew Lin <mlin367@yahoo.com>
2019-05-26 01:22:41 -0700

    used material UI on edit shoe fields

--- client/src/components/EditShoe.jsx
+++ client/src/components/EditShoe.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../css/EditShoe.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class EditShoe extends React.Component {
   constructor(props) {
@@ -45,48 +47,72 @@ class EditShoe extends React.Component {
     return (
       <div className="editshoe-modal">
         <div className="editshoe">
-          <span>Image URL: <input type="file" onChange={this.handleOnImageUpload} /></span>
-          or external link:
-          <input
+          <Button variant="contained" component="label">
+            Upload Image
+            <input
+              type="file"
+              style={{ display: 'none' }}
+              onChange={this.handleOnImageUpload}
+            />
+          </Button>
+          <br />
+          OR
+          <TextField
+            multiline
+            label="Image Link"
             type="url"
             onChange={e => this.onInputChange(e, 'image_url')}
             value={this.state.image_url}
           />
-          Name:{' '}
-          <input
+          <TextField
+            multiline
+            label="Name"
             onChange={e => this.onInputChange(e, 'name')}
             value={this.state.name}
           />
-          Brand:{' '}
-          <input
+          <TextField
+            multiline
+            label="Brand"
             onChange={e => this.onInputChange(e, 'brand')}
             value={this.state.brand}
           />
-          Style:{' '}
-          <input
+          <TextField
+            multiline
+            label="Style"
             onChange={e => this.onInputChange(e, 'style')}
             value={this.state.style}
           />
-          Size:
-          <input
+          <TextField
+            multiline
+            label="Size"
             onChange={e => this.onInputChange(e, 'size')}
             value={this.state.size}
           />
-          UPC ID:{' '}
-          <input
+          <TextField
+            multiline
+            label="UPC ID"
             onChange={e => this.onInputChange(e, 'upc_id')}
             value={this.state.upc_id}
           />
-          <button
-            onClick={() => {
-              if (this.props.onAddEditDelete(this.props.shoeId, { ...this.state }, true) === true) {
-                this.props.onCancel();
-              };
-            }}
-          >
-            Submit
-          </button>
-          <button onClick={this.props.onCancel}>Cancel</button>
+          <br />
+          <div className="editShoeButtons">
+            <Button
+              onClick={() => {
+                if (
+                  this.props.onAddEditDelete(
+                    this.props.shoeId,
+                    { ...this.state },
+                    true
+                  ) === true
+                ) {
+                  this.props.onCancel();
+                }
+              }}
+            >
+              Submit
+            </Button>
+            <Button onClick={this.props.onCancel}>Cancel</Button>
+          </div>
         </div>
       </div>
     );
